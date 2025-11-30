41fa1937258c6fe11f50aff7d94113d9e6b2eb77
husamuel <hugoleonor10@gmail.com>
2025-11-30 21:24:55 +0000

    cd

--- eks-project/terraform/outputs.tf
+++ eks-project/terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "ecr_repository_url" {
     value = aws_ecr_repository.my_repo.repository_url
 }
+
+output "sbom_bucket_url" {
+  description = "URL do bucket S3"
+  value       = "s3://${aws_s3_bucket.sbom_bucket.bucket}"
+}
---
+++ eks-project/terraform/s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "sbom_bucket" {
+  bucket = "sbom-storage-bucket"
+}
