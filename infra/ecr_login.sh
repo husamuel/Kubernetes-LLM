f24586b6d43512f844bf257141cda57b2034a652
husamuel <hugoleonor10@gmail.com>
2025-12-12 11:07:07 +0000

    cleaning runner

--- infra/ecr_login.sh
+++ infra/ecr_login.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
-aws ecr get-login-password --region us-east-1 | \
-kubectl delete secret ecr-secret --ignore-not-found
+TOKEN=$(aws ecr get-login-password --region us-east-1)
 
-aws ecr get-login-password --region us-east-1 | \
+kubectl delete secret ecr-secret -n gateway --ignore-not-found
 kubectl create secret docker-registry ecr-secret \
-  --docker-server=539358810919.dkr.ecr.us-east-1.amazonaws.com \
-  --docker-username=AWS \
-  --docker-password-stdin
+          --docker-server=539358810919.dkr.ecr.us-east-1.amazonaws.com \
+          --docker-username=AWS \
+          --docker-password=$TOKEN \
+          -n gateway
+
+TOKEN=$(aws ecr get-login-password --region us-east-1)
+
+kubectl delete secret ecr-secret -n llm --ignore-not-found
+kubectl create secret docker-registry ecr-secret \
+            --docker-server=539358810919.dkr.ecr.us-east-1.amazonaws.com \
+                --docker-username=AWS \
+                    --docker-password="$TOKEN" \
+                        -n llm
+
+TOKEN=$(aws ecr get-login-password --region us-east-1)
+
+kubectl delete secret ecr-secret -n storage --ignore-not-found
+kubectl create secret docker-registry ecr-secret \
+            --docker-server=539358810919.dkr.ecr.us-east-1.amazonaws.com \
+                --docker-username=AWS \
+                    --docker-password="$TOKEN" \
+                        -n storage
